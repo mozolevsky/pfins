47c8143779da023a3d2a73098c2fa9c2a4f443de
mozolevsky <mrmozolevsky@gmail.com>
2025-09-11 22:33:41 +0200

    server watch capabilities

--- server/index.ts
+++ server/index.ts
@@ -16,6 +16,7 @@ const mockedAssets = [
   { type: "Cash", value: 1000 },
   { type: "Stock", value: 2000 },
   { type: "Real Estate", value: 3000 },
+  { type: "Crypto", value: 4000 },
 ];
 
 const resolvers = {
