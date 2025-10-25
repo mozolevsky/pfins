4442662cd44469400e1bb4236258b83e2ecf04cf
mozolevsky <mrmozolevsky@gmail.com>
2025-10-25 21:17:46 +0200

    client: react compiler

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler', {
+            // React Compiler options
+            compilationMode: 'infer',
+            runtimeModule: 'react/compiler-runtime'
+          }]
+        ]
+      }
+    })
+  ],
 })
