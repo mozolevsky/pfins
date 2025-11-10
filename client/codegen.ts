c8eaef9690feaf6b66b78e00d595d69b5be682b7
mozolevsky <mrmozolevsky@gmail.com>
2025-11-10 23:43:52 +0100

    Add GraphQL codegen configuration and dependencies
    - Add codegen config files for client and server
    - Move server codegen.ts from src/ to root directory
    - Add GraphQL codegen dependencies to package.json
    - Update dev scripts to run codegen
    - Update yarn.lock with new dependencies

---
+++ client/codegen.ts
@@ -0,0 +1,23 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  // You can switch between these options:
+  schema: "http://localhost:4000", // Alternative: "../server/src/schema.graphql"
+  documents: ["src/**/*.{ts,tsx}"],
+  generates: {
+    "src/generated/graphql-types.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
+        apolloReactHooksImportFrom: '@apollo/client/react',
+      },
+    },
+  },
+};
+
+export default config;
+
